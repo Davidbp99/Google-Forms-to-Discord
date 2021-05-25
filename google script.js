5ac3714f580b1b800176d0ef0e96b85fdd311d9e
Davidbp99 <40523727+Davidbp99@users.noreply.github.com>
2021-05-25 19:28:12 +0200

    Made a config and edit how it works
    Made it so the user can just put in a discord hook into the form it self and send it.
    
    It got some stuff the user would need to edit to make sure its to what they like. That is very easy done in the config section (bot name did not want to be there. You can find it bit info the function)

--- google script.js
+++ google script.js
@@ -1,4 +1,14 @@
-var POST_URL = "WEBBHOOK URL";
+// Config area
+var Backupurl = ""; // For backup
+var Logo = "";// URL to the logo you want
+var bottittle = "";
+var colornice = "15745792";
+// End of Config area
+
+//System Stuff
+var WhatWentWrong = ""; // We need to find out what broke by combinding this with your form reply below.
+today_date = Utilities.formatDate(new Date(), "GMT-0", "dd/MM/yyyy");
+
 
 function onSubmit(e) {
     var form = FormApp.getActiveForm();
@@ -7,52 +17,83 @@ function onSubmit(e) {
     var response = latestResponse.getItemResponses();
     var items = [];
 
-    for (var i = 0; i < response.length; i++) {
-        var question = response[i].getItem().getTitle();
-        var answer = response[i].getResponse();
-        try {
-            var parts = answer.match(/[\s\S]{1,1024}/g) || [];
-        } catch (e) {
-            var parts = answer;
-        }
+    // To change the bot name (it did not want to be in the config area)
+var botname = "Edit This For Your Bot Name";
+//
 
-        if (answer == "") {
-            continue;
-        }
-        for (var j = 0; j < parts.length; j++) {
-            if (j == 0) {
-                items.push({
-                    "name": question,
-                    "value": parts[j],
-                    "inline": false
-                });
-            } else {
-                items.push({
-                    "name": question.concat(" (cont.)"),
-                    "value": parts[j],
-                    "inline": false
-                });
-            }
-        }
+      // We first need to find the discord hook the user will use
+  var answer = response[0].getResponse() // The items start at 0 and not 1 (keep that in mind)
+  // To get the actual text of the first questions answer, we need to filter the unnecessary characters out using regex.
+  try {
+    var parts = answer.match(/[\s\S]{1,1024}/g) || [];
+  } catch (e) {
+    var parts = answer;
+  }
+  var POST_URL = answer; // We need to update our POST_URL to what the form user typed in
+    if (parts == "") {
+      // For situations if someone would type nothing or fully forget about it.
+      var botname = "The Back Up hook was used";
+      var POST_URL = Backupurl;
+      var WhatWentWrong = "You Typed Nothing "+answer; 
     }
 
-    var options = {
-        "method": "post",
-        "headers": {
-            "Content-Type": "application/json",
-        },
-        "payload": JSON.stringify({
-            "content": "‌",
-            "embeds": [{
-                "title": "Some nice title here",
-              "color": 33023, // This is optional, you can look for decimal colour codes at https://www.webtoolkitonline.com/hexadecimal-decimal-color-converter.html
-                "fields": items,
-                "footer": {
-                    "text": "Some footer here"
-                }
-            }]
-        })
-    };
+      // This will then remove the hook being sent over discord
+  for (var i = 1; i < response.length; i++) {
+    var question = response[i].getItem().getTitle();
+    var answer = response[i].getResponse();
+    try {
+      var parts = answer.match(/[\s\S]{1,1024}/g) || [];
+    } catch (e) {
+      var parts = answer;
+    }
+    
+    if (answer == "") {
+      continue;
+    }
+    for (var j = 0; j < parts.length; j++) {
+      if (j == 0) {
+        items.push({
+          "name": question,
+          "value": parts[j],
+          "inline": false
+        });
+      } else {
+        items.push({
+          "name": question.concat(" (cont.)"),
+          "value": parts[j],
+          "inline": false
+        });
+      }
+    }
+  }
+var name = botname;
+   var options = {
+    "method": "post",
+    "contentType": "application/json",
+    "payload": JSON.stringify({
+      "avatar_url": Logo,
+      "username": name,
+      "content": WhatWentWrong, // This is not an empty string
+      "embeds": [{
+              "thumbnail": {
+      "url": Logo
+    },
+        "title": bottittle,
+        "color": colornice,
+        "fields": items,
+        "footer": {
+          "text": today_date,
+          "icon_url": Logo,
+          
+        }
+        
+    
+      }
+      
+      ]
+    })
+    
+  };
 
     UrlFetchApp.fetch(POST_URL, options);
 };
